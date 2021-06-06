55b9872bff18cb8a20884dab9b3ae9ee477fcdb7
Sreejith Kumar Mundayad <sreejith-kumar.mundayad@in.abb.com>
2021-06-07 01:27:30 +0530

    Added additional informations

--- PersonalProfile/main.js
+++ PersonalProfile/main.js
@@ -5,8 +5,6 @@ import bgUrl from './background.jpg';
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
-
-
 const scene  = new THREE.Scene();
 //setup the camera
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight, 0.1,1000);
