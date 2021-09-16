4dbfba1e28048c4859929c5abc2ee46a479b81b5
SREEJITH KUMAR <hardsree123@gmail.com>
2021-09-16 11:34:36 +0530

    Create readme.m
    added readme for my profile.

---
+++ readme.m
@@ -0,0 +1,13 @@
+- ### Hi there <a href="https://sreejithkumarm.vercel.app/"><img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="25px"></a>
+- 👀 I’m interested in developing cool and interesting stuffs.
+- 🌱 I’m currently working in .net and angular full stack, and learning react and javascript/typescript stack.
+- 💞️ Ask me anything on Angular, .net, React, Javscript. 
+- 📫 You can email me on hardsree123@gmail.com
+- 👨‍💻 &nbsp;Read more about me at [sreejithkumarm.vercel.app](https://sreejithkumarm.vercel.app/)
+
+🔗 &nbsp;**Connect with me**
+<p align="left">
+<a href="https://www.linkedin.com/in/sreejith-kumar-m/" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="sreejithkumarm" height="30" width="40" /></a>
+</p>
+
+
